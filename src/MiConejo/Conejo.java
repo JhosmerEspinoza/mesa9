6925b34e0b223295b1e946f205573dce8111f20d
Angel Cuenca <Angel_Cuenca_95@hotmail.com>
2021-09-21 19:25:16 -0300

    Añado el conejo

---
+++ src/MiConejo/Conejo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MiConejo;
+
+/**
+ *
+ * @author Angel
+ */
+public class Conejo {
+    private String name;
+    private String color;
+    private Integer age;
+
+    public Conejo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+}
