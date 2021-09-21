65bc8fbe07d0dab0a72a9a75afaba0b67f477bb7
Jose Espinoza <jhosmer2601@hotmail.com>
2021-09-21 20:00:43 -0300

    anadi un pato

---
+++ src/Entidades/Pato.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author jhosenny
+ */
+public class Pato {
+    private String nombre;
+    private String color;
+    private int edad;
+    private Boolean sexo;
+
+    public Pato() {
+    }
+    
+
+    public Pato(String nombre, String color, int edad, Boolean sexo) {
+        this.nombre = nombre;
+        this.color = color;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Boolean getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Boolean sexo) {
+        this.sexo = sexo;
+    }     
+}
