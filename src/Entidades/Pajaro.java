5e78e06ab871f65654322c5d1cd21fe7fb497006
Angel Cuenca <Angel_Cuenca_95@hotmail.com>
2021-09-21 19:59:19 -0300

    añadi el pajaro

---
+++ src/Entidades/Pajaro.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Angel
+ */
+public class Pajaro {
+    private String name;
+    private String color;
+    private Integer age;
+
+    public Pajaro() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    
+}
