f1bb6c5362f459aa37ae95f52cf07ce6c4c0c102
Jose Espinoza <jhosmer2601@hotmail.com>
2021-09-21 19:51:34 -0300

    Anado un gato

--- src/Entidades/Gato.java
+++ src/Entidades/Gato.java
@@ -11,5 +11,53 @@ package Entidades;
  * @author jhosenny
  */
 public class Gato {
+    private String nombre;
+    private String raza;
+    private Integer anhos;
+    private Float tamanho;
+
+    public Gato() {
+    }
+
+    public Gato(String nombre, String raza, Integer anhos, Float tamanho) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.anhos = anhos;
+        this.tamanho = tamanho;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public Integer getAnhos() {
+        return anhos;
+    }
+
+    public void setAnhos(Integer anhos) {
+        this.anhos = anhos;
+    }
+
+    public Float getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Float tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    
     
 }
