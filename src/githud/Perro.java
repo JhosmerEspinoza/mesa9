b0fe9e05f628730ea45596c7733b76ae9578b6b0
Angel Cuenca <Angel_Cuenca_95@hotmail.com>
2021-09-21 19:16:35 -0300

    Agregue el perro,por lo que hago un commit

---
+++ src/githud/Perro.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githud;
+
+/**
+ *
+ * @author Angel
+ */
+public class Perro {
+    private String name;
+    private String race;
+    private Integer age;
+    private Integer weight;
+
+    public Perro() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+    
+}
